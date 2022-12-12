43b7243468cb9ac82d0faa1bc31359405dafa379
Jas <theautomationtechies@gmail.com>
2022-12-11 21:58:09 -0500

    added all solutions

---
+++ A3/OpenLibraryAssignment/API/Request/Requests.cs
@@ -0,0 +1,87 @@
+﻿using Newtonsoft.Json;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using static OpenLibraryAPITests.API.Response.ResponseObject;
+
+namespace OpenLibraryAPITests.API.Request
+{
+    class Requests
+    {
+        private HttpClient httpClient = new HttpClient();
+
+        private string URI = "https://openlibrary.org/search.json?q=";
+
+        private string query = "Goodnight+Moon+123+lap";
+
+        public int RootObject { get; private set; }
+
+        public async Task<string> SanityTest()
+        {
+            var Builder = new System.UriBuilder($"{URI}" + query);
+
+            var response = await httpClient.GetAsync(Builder.Uri);
+
+            var content = await response.Content.ReadAsStringAsync();
+
+            Console.WriteLine(content);
+            return content;
+
+        }
+
+        public async Task<Rootobject> GetResponseFromAPI(string searchString)
+        {
+            string html;
+            string url = URI + searchString;
+            Console.WriteLine(url);
+
+            HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+            httpWebRequest.Method = "GET";
+            HttpWebResponse webResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+
+            Stream stream = webResponse.GetResponseStream();
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                html = reader.ReadToEnd();
+            }
+
+            var apiResponse = JsonConvert.DeserializeObject<Rootobject>(html);
+
+            return apiResponse;
+        }
+
+        public string GetResponseFromAPIInString(string searchString)
+        {
+            string html;
+            string url = URI + searchString;
+            Console.WriteLine(url);
+
+            HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+            httpWebRequest.Method = "GET";
+            HttpWebResponse webResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+
+            Stream stream = webResponse.GetResponseStream();
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                html = reader.ReadToEnd();
+            }   
+
+            return html;
+        }
+
+        public static string readJson(string fileName)
+        {
+            string json;
+            using (StreamReader r = new StreamReader(fileName))
+            {
+                json = r.ReadToEnd();                
+            }
+
+            return json;
+        }
+    }
+}
---
+++ A3/OpenLibraryAssignment/APITests/Tests.cs
@@ -0,0 +1,89 @@
+﻿using FluentAssertions;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+using OpenLibraryAPITests.API.Request;
+using System;
+using System.Collections;
+using System.Threading.Tasks;
+using static OpenLibraryAPITests.API.Response.ResponseObject;
+
+namespace OpenLibraryAPITests.APITests
+{
+    public class Tests
+    {
+        [Category("Test")]
+        [Test]
+        public async Task TC01_ApiIsUpAndRunning()
+        {
+            Requests api = new Requests();
+
+            var response = await api.SanityTest();
+        }
+
+        [Category("Test")]
+        [Test]
+        public async Task TC02_GetNumberOfBooks()
+        {
+            string searchString = "Goodnight Moon";
+
+            Requests api = new Requests();
+
+            var response = await api.GetResponseFromAPI(searchString);
+            Console.WriteLine(response.numFound);
+        }
+
+        [Category("Test")]
+        [Test]
+        public async Task TC03_GetListOfBooksSince2000()
+        {
+            string searchString = "Goodnight Moon";
+
+            Requests api = new Requests();
+
+            var response = await api.GetResponseFromAPI(searchString);
+            Console.WriteLine(response);
+            Doc[] docs = response.docs;
+            var keyList = new ArrayList();
+
+            foreach (Doc d in docs){
+                int first_publish_year = d.first_publish_year;
+                if (first_publish_year >= 2000)
+                {
+                    keyList.Add(d.key);
+                }
+
+            }
+            Console.WriteLine("Total Keys found : " + keyList.Count);
+
+            foreach (var key in keyList)
+            {
+                Console.WriteLine(key);
+            }
+
+        }
+
+        [Category("Test")]
+        [Test]
+        public async Task TC04_MatchResponse()
+        {
+            string expectedFilePath = @"D:\Work\Visual Studio Projects\OpenLibraryAssignment\Resources\expectResponse.json";
+            string notExpectedFilePath = @"D:\Work\Visual Studio Projects\OpenLibraryAssignment\Resources\notExpectedResponse.json";
+
+            string searchString = "Goodnight+Moon+123+Lap+Edition";
+
+            Requests api = new Requests();
+
+            var response = api.GetResponseFromAPIInString(searchString);
+            JToken actualJSON = JToken.Parse(response);
+            JToken expectedJSON = JToken.Parse(Requests.readJson(expectedFilePath));
+            JToken notExpectedJSON = JToken.Parse(Requests.readJson(notExpectedFilePath));
+
+            actualJSON.Should().BeEquivalentTo(expectedJSON);
+            //actualJSON.Should().BeEquivalentTo(notExpectedJSON);
+
+        }
+
+
+
+    }
+}
